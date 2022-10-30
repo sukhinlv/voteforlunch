dfdd105d36299f93b633cbcdfff729972ebb6b23
Leonid Sukhin <leva1981@yandex.ru>
2022-10-30 18:03:44 +0300

    First commit

---
+++ src/main/java/ru/jsft/voteforlunch/VoteforlunchApplication.java
@@ -0,0 +1,13 @@
+package ru.jsft.voteforlunch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoteforlunchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteforlunchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/jsft/voteforlunch/VoteforlunchApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.jsft.voteforlunch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoteforlunchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
