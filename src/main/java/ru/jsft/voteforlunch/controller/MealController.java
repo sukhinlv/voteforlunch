fc3f051d5d0f2ed97b24fadd6c73bd292f8bc21d
Leonid Sukhin <leva1981@yandex.ru>
2022-11-02 12:01:54 +0300

    add MealController basic CRUD operations

--- src/main/java/ru/jsft/voteforlunch/controller/MealController.java
+++ src/main/java/ru/jsft/voteforlunch/controller/MealController.java
@@ -1,10 +1,9 @@
 package ru.jsft.voteforlunch.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 import ru.jsft.voteforlunch.model.Meal;
-import ru.jsft.voteforlunch.service.MealService;
+import ru.jsft.voteforlunch.repository.MealRepository;
 
 import java.util.List;
 
@@ -12,14 +11,32 @@ import java.util.List;
 @RequestMapping("api/v1/meals")
 public class MealController {
 
-    private final MealService mealService;
+    private final MealRepository repository;
 
-    public MealController(MealService mealService) {
-        this.mealService = mealService;
+    public MealController(MealRepository repository) {
+        this.repository = repository;
     }
 
     @GetMapping
     public List<Meal> getMeals() {
-        return mealService.getAllMeals();
+        return repository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Meal createNewMeal(@RequestBody Meal meal) {
+        return repository.save(meal);
+    }
+
+    @DeleteMapping(path = "/{mealId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMeal(@PathVariable Long mealId) {
+        repository.deleteById(mealId);
+    }
+
+    @PutMapping(path = "/{mealId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateMeal(@PathVariable Long mealId, @RequestParam String name) {
+        repository.updateById(mealId, name);
     }
 }
--- src/main/java/ru/jsft/voteforlunch/repository/MealRepository.java
+++ src/main/java/ru/jsft/voteforlunch/repository/MealRepository.java
@@ -1,9 +1,17 @@
 package ru.jsft.voteforlunch.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ru.jsft.voteforlunch.model.Meal;
 
 @Repository
 public interface MealRepository extends JpaRepository<Meal, Long> {
+
+    @Modifying
+    @Transactional
+    @Query("UPDATE Meal m SET m.name = :name  WHERE m.id = :id")
+    void updateById(Long id, String name);
 }
