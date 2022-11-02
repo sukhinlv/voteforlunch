dcc3e5e9ebe4a2e4177bf2b974eda0a161350f73
Leonid Sukhin <leva1981@yandex.ru>
2022-11-02 10:35:34 +0300

    refactor populate to use actual dates

--- src/main/java/ru/jsft/voteforlunch/config/PopulateVotes.java
+++ src/main/java/ru/jsft/voteforlunch/config/PopulateVotes.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Configuration;
 import ru.jsft.voteforlunch.model.*;
 import ru.jsft.voteforlunch.repository.*;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.LocalTime;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class PopulateVotes {
@@ -27,57 +27,49 @@ public class PopulateVotes {
             Meal soup = new Meal("Soup");
             Meal pasta = new Meal("Pasta");
             Meal omelet = new Meal("omelet");
-            mealRepository.save(tea);
-            mealRepository.save(bread);
-            mealRepository.save(soup);
-            mealRepository.save(pasta);
-            mealRepository.save(omelet);
+            mealRepository.saveAll(List.of(tea, bread, soup, pasta, omelet));
 
             Restaurant cherryRestaurant = new Restaurant("Cherry");
             Restaurant aishaRestaurant = new Restaurant("Aisha");
-            restaurantRepository.save(cherryRestaurant);
-            restaurantRepository.save(aishaRestaurant);
+            restaurantRepository.saveAll(List.of(cherryRestaurant, aishaRestaurant));
 
             User admin = new User("admin", "admin", "admin@ya.ru", true,
                     LocalDate.of(2022, 10, 15), Collections.singleton(Role.ADMIN));
             User user = new User("user", "user", "user@gmail.com", true,
                     LocalDate.of(2022, 10, 20), Collections.singleton(Role.USER));
-            userRepository.save(admin);
-            userRepository.save(user);
+            userRepository.saveAll(List.of(admin, user));
+
+            LocalDate nowMinusTwoDays = LocalDate.now().minusDays(2);
+            LocalDate nowMinusOneDay = LocalDate.now().minusDays(1);
 
             Menu menuForCherry1 = new Menu();
-            menuForCherry1.setDateOfMenu(LocalDate.of(2022, 10, 25));
+            menuForCherry1.setDateOfMenu(nowMinusTwoDays);
             menuForCherry1.setRestaurant(cherryRestaurant);
-            menuForCherry1.getMealPrice().add(new MealPrice(tea, BigDecimal.valueOf(10)));
-            menuForCherry1.getMealPrice().add(new MealPrice(bread, BigDecimal.valueOf(15)));
+            menuForCherry1.getMealPrice().add(new MealPrice(tea, 10));
+            menuForCherry1.getMealPrice().add(new MealPrice(bread, 15));
             Menu menuForCherry2 = new Menu();
-            menuForCherry2.setDateOfMenu(LocalDate.of(2022, 10, 26));
+            menuForCherry2.setDateOfMenu(nowMinusOneDay);
             menuForCherry2.setRestaurant(cherryRestaurant);
-            menuForCherry2.getMealPrice().add(new MealPrice(soup, BigDecimal.valueOf(25)));
-            menuForCherry2.getMealPrice().add(new MealPrice(bread, BigDecimal.valueOf(15)));
+            menuForCherry2.getMealPrice().add(new MealPrice(soup, 25));
+            menuForCherry2.getMealPrice().add(new MealPrice(bread, 15));
             Menu menuForAisha1 = new Menu();
-            menuForAisha1.setDateOfMenu(LocalDate.of(2022, 10, 25));
+            menuForAisha1.setDateOfMenu(nowMinusTwoDays);
             menuForAisha1.setRestaurant(aishaRestaurant);
-            menuForAisha1.getMealPrice().add(new MealPrice(tea, BigDecimal.valueOf(10)));
-            menuForAisha1.getMealPrice().add(new MealPrice(pasta, BigDecimal.valueOf(30)));
+            menuForAisha1.getMealPrice().add(new MealPrice(tea, 10));
+            menuForAisha1.getMealPrice().add(new MealPrice(pasta, 30));
             Menu menuForAisha2 = new Menu();
-            menuForAisha2.setDateOfMenu(LocalDate.of(2022, 10, 26));
+            menuForAisha2.setDateOfMenu(nowMinusOneDay);
             menuForAisha2.setRestaurant(aishaRestaurant);
-            menuForAisha2.getMealPrice().add(new MealPrice(tea, BigDecimal.valueOf(10)));
-            menuForAisha2.getMealPrice().add(new MealPrice(omelet, BigDecimal.valueOf(20)));
-            menuRepository.save(menuForCherry1);
-            menuRepository.save(menuForCherry2);
-            menuRepository.save(menuForAisha1);
-            menuRepository.save(menuForAisha2);
+            menuForAisha2.getMealPrice().add(new MealPrice(tea, 10));
+            menuForAisha2.getMealPrice().add(new MealPrice(omelet, 20));
+            menuRepository.saveAll(List.of(menuForCherry1, menuForCherry2, menuForAisha1, menuForAisha2));
 
-            voteRepository.save(new Vote(admin, cherryRestaurant, LocalDate.of(2022, 10, 25),
-                    LocalTime.of(9, 30)));
-            voteRepository.save(new Vote(admin, cherryRestaurant, LocalDate.of(2022, 10, 26),
-                    LocalTime.of(10, 15)));
-            voteRepository.save(new Vote(user, cherryRestaurant, LocalDate.of(2022, 10, 25),
-                    LocalTime.of(10, 45)));
-            voteRepository.save(new Vote(user, aishaRestaurant, LocalDate.of(2022, 10, 26),
-                    LocalTime.of(10, 0)));
+            voteRepository.saveAll(List.of(
+                    new Vote(admin, cherryRestaurant, nowMinusTwoDays, LocalTime.of(9, 30)),
+                    new Vote(admin, cherryRestaurant, nowMinusOneDay, LocalTime.of(10, 15)),
+                    new Vote(user, cherryRestaurant, nowMinusTwoDays, LocalTime.of(10, 45)),
+                    new Vote(user, aishaRestaurant, nowMinusOneDay, LocalTime.of(10, 0))
+            ));
         };
     }
 }
