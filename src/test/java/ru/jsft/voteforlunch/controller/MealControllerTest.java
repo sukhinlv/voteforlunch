9482a4e8f8403426e811b0df6562c570dfba4dda
Leonid Sukhin <leva1981@yandex.ru>
2022-11-02 14:46:21 +0300

    create MealControllerTest and first test method

---
+++ src/test/java/ru/jsft/voteforlunch/controller/MealControllerTest.java
@@ -0,0 +1,44 @@
+package ru.jsft.voteforlunch.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import ru.jsft.voteforlunch.model.Meal;
+
+import java.util.Objects;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static ru.jsft.voteforlunch.utils.ObjectUtils.objectToJson;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class MealControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void itShouldCreateMeal() throws Exception {
+        // Given
+        Meal meal = new Meal("Some test meal");
+
+        // When 
+        ResultActions performCreateNewMeal = mockMvc.perform(post("/api/v1/meals")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Objects.requireNonNull(objectToJson(meal))));
+
+        // Then
+        performCreateNewMeal.andExpect(status().isCreated());
+
+        String jsonResponse = performCreateNewMeal.andReturn().getResponse().getContentAsString();
+        Meal responseMeal = new ObjectMapper().readValue(jsonResponse, Meal.class);
+        assertThat(responseMeal).usingRecursiveComparison().ignoringFields("id").isEqualTo(meal);
+    }
+}
