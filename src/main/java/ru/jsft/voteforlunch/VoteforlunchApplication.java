c15eeceb0220aa36f907a55f0a84be3ebc28e530
Leonid Sukhin <leva1981@yandex.ru>
2022-11-07 13:42:02 +0300

    add Caching and Tomcat connection pool

--- src/main/java/ru/jsft/voteforlunch/VoteforlunchApplication.java
+++ src/main/java/ru/jsft/voteforlunch/VoteforlunchApplication.java
@@ -2,8 +2,10 @@ package ru.jsft.voteforlunch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class VoteforlunchApplication {
 
     public static void main(String[] args) {
