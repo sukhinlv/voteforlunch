ed78e730b2f298c6b82a5d1da95089ff34750ed5
Leonid Sukhin <leva1981@yandex.ru>
2022-11-28 14:15:04 +0300

    add utility classes JsonUtil and MatcherFactory

--- src/main/java/ru/jsft/voteforlunch/config/AppConfig.java
+++ src/main/java/ru/jsft/voteforlunch/config/AppConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import ru.jsft.voteforlunch.web.util.JsonUtil;
 
 import java.time.Clock;
 
@@ -14,6 +15,7 @@ public class AppConfig {
     @Autowired
     void configureAndStoreObjectMapper(ObjectMapper objectMapper) {
         objectMapper.registerModule(new Hibernate5Module());
+        JsonUtil.setMapper(objectMapper);
     }
 
     @Bean
