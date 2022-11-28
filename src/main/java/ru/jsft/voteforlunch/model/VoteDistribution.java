0fcd877b17c0ba697f80b9bf64fa17d28b12e033
Leonid Sukhin <leva1981@yandex.ru>
2022-11-28 12:40:07 +0300

    change type of VoteDistribution to record

--- src/main/java/ru/jsft/voteforlunch/model/VoteDistribution.java
+++ src/main/java/ru/jsft/voteforlunch/model/VoteDistribution.java
@@ -1,14 +1,7 @@
 package ru.jsft.voteforlunch.model;
 
-import lombok.*;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Getter
-@Setter
-@ToString(callSuper = true)
-public class VoteDistribution {
-    private long restaurantId;
-    private String restaurantName;
-    private long voteCount;
-}
+public record VoteDistribution (
+    long restaurantId,
+    String restaurantName,
+    long voteCount
+) {}
