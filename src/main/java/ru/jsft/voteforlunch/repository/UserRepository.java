89856391b7edc82a558199c66f2004b0518c287b
Leonid Sukhin <leva1981@yandex.ru>
2022-12-19 14:57:48 +0300

    refactor UserRepository

--- src/main/java/ru/jsft/voteforlunch/repository/UserRepository.java
+++ src/main/java/ru/jsft/voteforlunch/repository/UserRepository.java
@@ -2,11 +2,13 @@ package ru.jsft.voteforlunch.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 import ru.jsft.voteforlunch.model.User;
 
 import java.util.Optional;
 
+@Repository
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Long> {
 
